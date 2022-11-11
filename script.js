ae16d378803e23f74138e66188bd86905cec3bd6
Mariuskvaal <mariuskvaal1@gmail.com>
2022-11-11 12:02:36 +0100

    updtaing css and js for the carosell

---
+++ script.js
@@ -0,0 +1,17 @@
+const blogContainers = document.querySelectorAll(".blog-container");
+
+const nxtBtn = document.querySelectorAll(".nxt-btn");
+const preBtn = document.querySelectorAll(".pre-btn");
+
+blogContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener("click", () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener("click", () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
