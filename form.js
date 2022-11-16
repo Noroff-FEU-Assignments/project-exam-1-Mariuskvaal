72a4daa2d49e6c38a73e640f434d7e1dd47155c1
Mariuskvaal <mariuskvaal1@gmail.com>
2022-11-16 10:33:14 +0100

    updatin contact form

--- form.js
+++ form.js
@@ -1,3 +1,94 @@
+const form = document.querySelector("#contactForm");
+
+const name = document.querySelector("#name");
+const nameError = document.querySelector("#nameError");
+
+const email = document.querySelector("#email");
+const emailError = document.querySelector("#emailError");
+
+const subject = document.querySelector("#subject");
+const subjectError = document.querySelector("#subjectError")
+
+function validateForm(){
+    event.preventDefault();
+
+    if (checkLength(name.value, 0) === true) {
+        nameError.style.display = "none";
+    } else {
+        nameError.style.display = "block";
+    }
+
+    if (validateEmail(email.value) === true) {
+        emailError.style.display = "none";
+    } 
+    else {
+        emailError.style.display = "block";
+    }
+
+    if(subject.value.trim().length > 5) {
+        subjectError.style.display = "none";
+    } else {
+        subjectError.style.display = "block";
+    }
+
+    if(message.value.trim().length > 20) {
+        messageError.style.display = "none";
+    } else {
+        messageError.style.display = "block";
+    }
+}
+
+
+form.addEventListener("submit", validateForm);
+
+function checkLength(value, len) {
+    if (value.trim().length > len) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validateEmail(email) {
+    const regEx = /\S+@\S+\.\S+/;
+    const patternMatches = regEx.test(email);
+    return patternMatches;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
 const form = document.querySelector(".formclass");
 const name = document.querySelector("#name");
 const email = document.querySelector("#email");
