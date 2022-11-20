79a287fd24eac8e97fff48b704f2050bc6472fd0
Mariuskvaal <mariuskvaal1@gmail.com>
2022-11-20 15:10:01 +0100

    update

--- json.js
+++ json.js
@@ -1,7 +1,12 @@
 baseUrl="https://taktilbehor.com/wp-json/wp/v2/posts";
 
-
-async function getProducts(url){
+async function getBlogs(url){
     const response = await fetch(url);
-
+    const blogs = await response.json();
+    blogs.forEach(function(blog){
+        blogContainer.innerHTML += <div class="blog"><h2>${blog.name}</h2></div>
+        <div class="blog-image" style="background-image:url('${blog.images[0].src}'"></div>
+    })
 }
+
+getBlogs(baseUrl);
