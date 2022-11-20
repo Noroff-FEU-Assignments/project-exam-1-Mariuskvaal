4e83f0555b845aa9c9344c09cd056708727a0d5f
Mariuskvaal <mariuskvaal1@gmail.com>
2022-11-20 16:15:46 +0100

    updating

--- json.js
+++ json.js
@@ -1,12 +1,22 @@
-baseUrl="https://taktilbehor.com/wp-json/wp/v2/posts";
+const baseUrl ="https://taktilbehor.com/wp-json/wp/v2/posts";
+
+const blogContainer = document.querySelector(".blogs");
 
 async function getBlogs(url){
+
     const response = await fetch(url);
+
     const blogs = await response.json();
+
     blogs.forEach(function(blog){
-        blogContainer.innerHTML += <div class="blog"><h2>${blog.name}</h2></div>
-        <div class="blog-image" style="background-image:url('${blog.images[0].src}'"></div>
-    })
+        blogContainer.innerHTML += "
+        <h2> Hello world </h2>
+        "
+        
+    });
+
+
+
 }
 
 getBlogs(baseUrl);
