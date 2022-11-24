73af6c1e9b316d43ac93eadb4defbfc632583f8f
Mariuskvaal <mariuskvaal1@gmail.com>
2022-11-24 22:13:28 +0100

    java update

--- json.js
+++ json.js
@@ -15,13 +15,13 @@ async function getBlogs() {
     console.log(json);
 
     for (let i = 0; i < json.length; i++) {
-        console.log(json[i]);
+        console.log(json[i].title.rendered);
 
         Container.innerHTML += `<a href="details.html?id=${json[i].id}" class="card">
                                 <div id="small-wrapper">
-                                <h4 class="overskrifblogg">${json[i].slug}</h4>
+                                <h4 class="overskrifblogg">${json[i].title.rendered}</h4>
                                 <div class="rendered"> ${json[i]} </div>
-                                <div class="rendered"> ${json[i].excerpt.rendered} </div>
+                                <p class="descritopn" style=text-decoration="none"> ${json[i].excerpt.rendered} </p>
                                 </div>
 
         
