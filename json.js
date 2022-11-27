ad96289002818190ee7abee7165e51213dd9a243
Mariuskvaal <mariuskvaal1@gmail.com>
2022-11-27 20:53:39 +0100

    updating the javascript

--- json.js
+++ json.js
@@ -2,7 +2,12 @@ const Container = document.querySelector(".content");
 
 const url ="https://taktilbehor.com/wp-json/wp/v2/posts?_embed";
 
-const loadMoreButton = document.querySelector(".load-more-button");
+const first10Posts = "https://taktilbehor.com/wp-json/wp/v2/posts?_embed";
+
+const loadMorePosts = "https://taktilbehor.com/wp-json/wp/v2/posts?page=2"
+
+
+
 
 async function getBlogs() {
 
@@ -12,7 +17,7 @@ async function getBlogs() {
 
     const json = await response.json();
 
-    console.log(json);
+    //console.log(json);
 
     for (let i = 0; i < json.length; i++) {
         console.log(json[i]);
@@ -20,11 +25,6 @@ async function getBlogs() {
         Container.innerHTML += `<a class="card" href="details.html?id=${json[i].id}" >
                                 <h2 class="overskrifblogg">${json[i].title.rendered}</h2>
                                 <div class="rendered"> ${json[i].content.rendered} </div>
-                        
-
-        
-        
-
     </a>` ;
     }                                                                                     
  }
@@ -36,3 +36,37 @@ async function getBlogs() {
 
 getBlogs();
 
+
+    
+   async function getMoreBlogs() {
+   
+   const moreRespone = await fetch(loadMorePosts);
+
+   const moreJson = await moreRespone.json();
+
+   loadMoreButton.onclick = () => {
+
+   for (let i = 0; i < moreJson.length; i++) {
+      console.log(moreJson[i]);
+
+      Container.innerHTML += `<a class="card" href="details.html?id=${moreJson[i].id}" >
+      <h2 class="overskrifblogg">${moreJson[i].title.rendered}</h2>
+      <div class="rendered"> ${moreJson[i].content.rendered} </div>
+</a> `
+
+;
+   }
+
+   loadMoreButton.style.display = "none";
+   
+
+
+}
+};
+
+getMoreBlogs();
+
+
+const loadMoreButton = document.querySelector(".load-more-button");
+
+const buttonWrapper = document.querySelector(".button-wrapper");
